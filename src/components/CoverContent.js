be51978a52150f29ac68d38b128a0b73cc31fd52
Arum <arum.adiningtyas@gmail.com>
2022-09-19 10:52:51 +0700

    Updates

--- src/components/CoverContent.js
+++
@@ -1,8 +0,0 @@
-export const CoverContent = ({className, title, subtitle}) => {
-    return (
-        <div className={'cover__content ' + className}>
-            <span className="cover__title">{title}</span>
-            <p>{subtitle}</p>
-        </div>
-    )
-  }
